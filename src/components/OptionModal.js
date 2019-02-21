c0800b4e9b890308287cf7f72207aca0c2613c1c
eternale88 <ethanrobbins@ymail.com>
2019-02-21 13:58:36 -0500

    fix

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,7 +3,7 @@ import Modal from 'react-modal';
 
 const OptionModal = (props) => {
   return (
-    <Modal isOpen={!!props.selectedOption} contentLable="Selected Option">
+    <Modal isOpen={!!props.selectedOption} contentLabel="Selected Option">
       <h3>Selected Option</h3>
       {props.selectedOption && <p>{props.selectedOption}</p>}
       <button onClick={props.handleClearSelectedOption}>Okay</button>
